16f073209fd971d50787a6d0475b7b6e8d0fc494
Steve Tyler <steve.tyler@centrica.com>
2024-04-22 17:43:17 +0100

    section 80

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,12 +7,22 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex ) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map(innerArr => [...innerArr])]; // update object state immutably
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return <ol id="game-board">
-    {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+    {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
       <ol>
         {row.map((playerSymbol, colIndex) => (
           <li key={colIndex}>
-            <button>{playerSymbol}</button>
+            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
           </li>
         ))}
       </ol>
